4d3d9aeb86c9032df179bd1802054f379e20e681
Tristan Elliott <tristanelliott7@gmail.com>
2020-07-08 23:47:57 -0300

    changes

--- src/components/Services/ticketMaster.js
+++ src/components/Services/ticketMaster.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async function fetching(){
-  let response = await axios.get('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&size=2&dmaId=527&apikey=z8pipqaTCCTQc1R0OGDLl4bnygjdiXH3')
+  let response = await axios.get('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&size=2&dmaId=527&apikey=')
   console.log(response.data._embedded.events)
   return response.data._embedded.events
 }
